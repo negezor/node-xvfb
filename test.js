7e6619e589426d9561bc75b2cce8d4d5d23c721f
Negezor <negezor@gmail.com>
2024-06-16 05:32:55 +1100

    chore: replace 4 spaces to 2

--- test.js
+++ test.js
@@ -8,22 +8,22 @@ console.error('started sync');
 xvfb.stopSync();
 console.error('stopped sync');
 xvfb.start(function(err) {
+  assert.equal(err, null);
+  console.error('started async');
+  xvfb.stop(function(err) {
     assert.equal(err, null);
-    console.error('started async');
-    xvfb.stop(function(err) {
+    console.error('stopped async');
+    xvfb.start(function(err) {
+      assert.equal(err, null);
+      console.error('started async');
+      xvfb.stopSync();
+      console.error('stopped sync');
+      xvfb.startSync();
+      console.error('started sync');
+      xvfb.stop(function(err) {
         assert.equal(err, null);
         console.error('stopped async');
-        xvfb.start(function(err) {
-            assert.equal(err, null);
-            console.error('started async');
-            xvfb.stopSync();
-            console.error('stopped sync');
-            xvfb.startSync();
-            console.error('started sync');
-            xvfb.stop(function(err) {
-                assert.equal(err, null);
-                console.error('stopped async');
-            });
-        });
+      });
     });
+  });
 });
